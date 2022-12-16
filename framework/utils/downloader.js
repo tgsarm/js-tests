506f1ee256f2d45f81c226834436fbe8ca2c0c76
Rustam Abdunasyrov <forze199@list.ru>
2022-12-16 13:59:54 +0300

    Add censorship

--- framework/utils/downloader.js
+++ framework/utils/downloader.js
@@ -6,10 +6,6 @@ class Downloader {
 
     async downloadImage(url, savePath) {
         const fullPath = await path.resolve(savePath);
-
-        console.log("пидор");
-        console.log(fullPath);
-
         logger.logInfo(`Downloading image from ${url}`);
         return download.image({url: url, dest: fullPath});
     }
